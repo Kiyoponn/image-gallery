dd9c5cf74355a1c8c6bfbee240dfb684fd407aad
Raju Khattri <rajukhattri.86@gmail.com>
2023-02-12 21:41:14 +0530

    Update

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,10 +1,11 @@
 import { createClient } from 'next-sanity'
 import { cache } from 'react'
-import { dataset, projectId } from './sanity.api'
+import { apiVersion, dataset, projectId } from './sanity.api'
 
 const clientConfig = {
   projectId,
   dataset,
+  apiVersion,
   useCdn: false,
 }
 
