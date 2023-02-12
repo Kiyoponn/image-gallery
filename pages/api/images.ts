c4f6c2b964ed90f41dd89cc1cc41c9a284431d1c
Raju Khattri <rajukhattri.86@gmail.com>
2023-02-12 21:28:48 +0530

    Update

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,23 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import client from '@/lib/sanity.client'
-import { ImageDataType } from '@/utils/Types'
+import { client } from '@/lib/sanity.client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { groq } from 'next-sanity'
 
 function getImagesQuery() {
   return client.fetch(groq`
-    *[_type == "gallery"] {
-      _id,
-      title,
-      ratio,
-      category,
-      "imageUrl": image.asset->url,
-      "alt": image.alt
-    }
-  `)
+    *[_type == "gallery"]`)
 }
 
 export default async function GET(_: NextApiRequest, res: NextApiResponse) {
-  const images: Array<ImageDataType> = await getImagesQuery()
+  const images = await getImagesQuery()
   res.status(200).json(images)
 }
