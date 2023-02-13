3408893e3f2b0697891a37db9b13dbc2cdf116d4
Raju Khattri <rajukhattri.86@gmail.com>
2023-02-13 16:22:57 +0530

    Update

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -3,5 +3,9 @@
 import { ThemeProvider } from 'next-themes'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider enableSystem={false} defaultTheme='dark' attribute='class'>{children}</ThemeProvider>
+  return (
+    <ThemeProvider enableSystem={false} defaultTheme='dark' attribute='class'>
+      {children}
+    </ThemeProvider>
+  )
 }
